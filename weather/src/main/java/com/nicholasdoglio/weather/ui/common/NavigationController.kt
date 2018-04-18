2baaa9232967f9d7175c2dd7f5aced269bc017ea
WhosNickDoglio <NicholasDoglio@Gmail.com>
2018-04-18 17:27:31 -0400

    Implemented Places API with AutoCompleteFragment

--- weather/src/main/java/com/nicholasdoglio/weather/ui/common/NavigationController.kt
+++ weather/src/main/java/com/nicholasdoglio/weather/ui/common/NavigationController.kt
@@ -1,39 +1,71 @@
 package com.nicholasdoglio.weather.ui.common
 
+import com.google.android.gms.common.api.Status
+import com.google.android.gms.location.places.AutocompleteFilter
+import com.google.android.gms.location.places.Place
+import com.google.android.gms.location.places.ui.PlaceSelectionListener
+import com.google.android.gms.location.places.ui.SupportPlaceAutocompleteFragment
 import com.nicholasdoglio.weather.R
 import com.nicholasdoglio.weather.ui.MainActivity
+import com.nicholasdoglio.weather.ui.forecast.ForecastFragment
 import com.nicholasdoglio.weather.ui.list.WeatherListFragment
-import com.nicholasdoglio.weather.ui.search.SearchFragment
+import timber.log.Timber
 import javax.inject.Inject
+
 
 /**
  * @author Nicholas Doglio
  */
-class NavigationController @Inject constructor(mainActivity: MainActivity) {
+class NavigationController @Inject constructor(private val mainActivity: MainActivity) {
     private val containerId = R.id.fragmentContainer
     private val fragmentManager = mainActivity.supportFragmentManager
 
-    fun openSearch() {
-        val searchFragment = SearchFragment()
+    //TODO transitions
+    fun openWeatherListFragment(latitude: Double = 0.0, longitude: Double = 0.0) {
         fragmentManager.beginTransaction()
-            .replace(containerId, searchFragment)
+            .replace(containerId, WeatherListFragment.create(latitude, longitude))
+            .commitAllowingStateLoss()
+    }
+
+    fun openOnboardingFragment() {
+
+    }
+
+    fun openForecastFragment(id: Int) {
+        fragmentManager.beginTransaction()
+            .replace(containerId, ForecastFragment.create(id))
             .addToBackStack(null)
             .commitAllowingStateLoss()
     }
 
-    fun openList() {
-        val listFragment = WeatherListFragment()
+    fun openAboutFragment() {
+        //TODO Dialog fragment
+    }
+
+    fun openSearchFragment() {
+        //TODO can I make this better?
+        val placesFragment = SupportPlaceAutocompleteFragment()
+
+        val typeFilter = AutocompleteFilter.Builder()
+            .setTypeFilter(AutocompleteFilter.TYPE_FILTER_CITIES)
+            .build()
+
+        placesFragment.setFilter(typeFilter)
+
+        placesFragment.setOnPlaceSelectedListener(object : PlaceSelectionListener {
+            override fun onPlaceSelected(place: Place?) {
+                Timber.d(("lat: ${place?.latLng?.latitude} long: ${place?.latLng?.longitude}"))
+                openWeatherListFragment(place!!.latLng.latitude, place.latLng.longitude)
+            }
+
+            override fun onError(error: Status?) {
+                Timber.d("${error?.statusCode}")
+            }
+        })
+
         fragmentManager.beginTransaction()
-            .replace(containerId, listFragment)
-            .commitAllowingStateLoss()
-
-    }
-
-    fun openDetail() {
-
-    }
-
-    fun openSettings() {
-
+            .replace(containerId, placesFragment)
+            .addToBackStack(null)
+            .commit()
     }
 }
