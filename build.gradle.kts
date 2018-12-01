e3bada51f643c20b552310d71e84504a4dc542ad
Nicholas Doglio <NicholasDoglio@Gmail.com>
2018-12-01 10:36:33 -0500

    REFACTOR: moved root build.gradle file to Kotlin DSL

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,12 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-
 buildscript {
     repositories {
         google()
         jcenter()
     }
     dependencies {
-        classpath 'com.android.tools.build:gradle:3.3.0-rc01'
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.10"
+        classpath("com.android.tools.build:gradle:3.3.0-rc01")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.10")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle.kts files
@@ -18,7 +17,7 @@ plugins {
     id("jmfayard.github.io.gradle-kotlin-dsl-libs") version "0.2.6"
 }
 
-apply from: 'dependencies.gradle'
+apply("dependencies.gradle")
 
 allprojects {
     repositories {
@@ -27,6 +26,6 @@ allprojects {
     }
 }
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
+tasks.register("clean", Delete::class.java) {
+    delete(rootProject.buildDir)
 }
