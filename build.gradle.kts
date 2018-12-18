b99d1d3a5cbb63bd9fb3ca01ac16c352633b6d11
Nicholas Doglio <NicholasDoglio@Gmail.com>
2018-12-17 21:46:43 -0500

    Upgraded to AGP 3.4.0-alpha08 and Gradle 5.1 RC2

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
         jcenter()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:3.4.0-alpha07")
+        classpath("com.android.tools.build:gradle:3.4.0-alpha08")
         classpath(kotlin("gradle-plugin", version = "1.3.11"))
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle.kts files
