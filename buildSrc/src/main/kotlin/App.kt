f1208fd9831be772ce0e8c4822e4504f66b0089a
Nicholas Doglio <NicholasDoglio@Gmail.com>
2018-12-18 21:11:26 -0500

    Updated Libs

---
+++ buildSrc/src/main/kotlin/App.kt
@@ -0,0 +1,7 @@
+object App {
+  const val minsdk: Int = 21
+  const val compilesdk: Int = 28
+  const val targetsdk: Int = 28
+  const val verisonCode: Int = 1
+  const val versionName: String = "1.0"
+}
